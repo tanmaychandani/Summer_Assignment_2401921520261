905fdfa8401e9e92211fde184f40b8055a559e16
tanmaychandani <tanmaychandani3@gmail.com>
2026-06-02 00:59:11 +0530

    Problem_1

--- WEEK_1/DAY_2/MaximumSubarray.java
+++ WEEK_1/DAY_2/MaximumSubarray.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = nums[0];
 
+        for(int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+
+            if(currSum > maxSum) {
+                maxSum = currSum;
+            }
+
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
